197fa4d9ee407ae3ad568b9833efa9925fe4da76
Noel Niles <noelniles@gmail.com>
2019-04-24 23:03:24 -1000

    Fiz point removal

--- camtrak.py
+++ camtrak.py
@@ -3,6 +3,7 @@ import hashlib
 import os
 import cv2
 import json
+from functools import partial
 import numpy as np
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
@@ -19,23 +20,22 @@ class Scene(QtWidgets.QGraphicsScene):
         y = ev.scenePos().y()
         self.image_points_updated.emit((x, y))
 
+
 class CamTrak(QtWidgets.QMainWindow):
     def __init__(self, cap):
         super(CamTrak, self).__init__()
         uic.loadUi('./gui/camtrak.ui',self)
+        self.camera = cap
         self.view_btn.clicked.connect(self.start_webcam)
-        self.capture_btn.clicked.connect(self.capture_image)
 
         self.scene = Scene(self.gview)
-        self.scene.image_points_updated.connect(self.add_known_image_points)
         self.gview.setScene(self.scene)
-        self.timer = QtCore.QTimer(self, interval=5)
+        self.npimage = None
+        self.connect_signals()
+        self.timer = QtCore.QTimer(self, interval=0.1)
         self.timer.timeout.connect(self.update_frame)
         self._image_counter = 0
         self.zoom = 1
-
-        self.capture = cap
-        self.npimage = None
 
         self.known_image_points = []
         self.tracking_mode = False
@@ -44,12 +44,33 @@ class CamTrak(QtWidgets.QMainWindow):
         self.known_geo_points_tbl.setColumnCount(3)
         self.known_geo_points_tbl.setHorizontalHeaderLabels(["Lat", "Lon", "Alt"])
 
-        # Connect PnP button
-        self.solve_pnp_btn.clicked.connect(self.solve_pnp)
+        self.param_file = None
 
-        # Connect the menu actions.
+        # Load the last session.
+        self.load_previous_session()
+
+    def connect_signals(self):
+        self.capture_btn.clicked.connect(self.capture_image)
+        self.scene.image_points_updated.connect(self.add_known_image_points)
+        self.solve_pnp_btn.clicked.connect(self.solve_pnp)
         self.action_save_parameters.triggered.connect(self.on_save_parameters)
         self.action_load_parameters.triggered.connect(self.on_load_parameters)
+
+    #def init_worker(self, camera):
+    #    worker = MultiThreadedGrabber(camera)
+    #    worker.new_image.connect(self.update_frame)
+    #    self.threadpool.start(worker)
+
+    def load_previous_session(self):
+        path = './previous_session.json'
+
+        if os.path.exists(path):
+            with open(path) as f:
+                prev_session = json.load(f)
+
+            prev_session_param_file = prev_session['param file']
+
+            self.on_load_parameters(prev_session_param_file)
 
     def on_save_parameters(self):
         obj_points = self.get_object_points()
@@ -70,42 +91,65 @@ class CamTrak(QtWidgets.QMainWindow):
             f.write(jsn)
 
         self.statusBar().showMessage(f'Parameters have been save to {fn}.')
+        self.param_file = fn
 
-    def on_load_parameters(self):
-        path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Choose a parameter file.", "", "JSON Files (*.json)")
+    def on_load_parameters(self, filename=None):
+        if filename is None:
+            path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Choose a parameter file.", "", "JSON Files (*.json)")
+        else:
+            path = filename
 
-        with open(path, 'r') as f:
+        if path == '' or path is None:
+            return
+
+        self.param_file = path
+
+        with open(self.param_file, 'r') as f:
             params = json.loads(f.read())
 
-        print(params)
         obj_points = params['object positions']
-        cam_pos = params['camera positions']
+        cam_pos    = params['camera positions']
         dist_coeff = params['distortion coefficients']
 
         for p in obj_points:
             x, y = p['x'], p['y']
-            self.add_known_image_points((x, y))
+            lat, lon, alt = p['lat'], p['lon'], p['alt']
+            self.add_known_image_points((x, y), latlonalt=(lat, lon, alt))
 
-        lat = cam_pos['lat']
-        self.camera_lat_line.setText(lat)
+        self.camera_lat_line.setValue(float(cam_pos['lat']))
+        self.camera_lon_line.setValue(float(cam_pos['lon']))
+        self.camera_alt_line.setValue(float(cam_pos['alt']))
+        self.cx_line.setValue(float(cam_pos['cx']))
+        self.cy_line.setValue(float(cam_pos['cy']))
+        self.phi_line.setValue(float(cam_pos['phi']))
+        self.theta_line.setValue(float(cam_pos['theta']))
+        self.psi_line.setValue(float(cam_pos['psi']))
 
-    def get_distortion_coeeficients(self):
-        k1 = float(self.k1_line.text())
-        k2 = float(self.k2_line.text())
-        k3 = float(self.k3_line.text())
-        p1 = float(self.p1_line.text())
-        p2 = float(self.p2_line.text())
+        self.k1_line.setValue(float(dist_coeff['k1']))
+        self.k2_line.setValue(float(dist_coeff['k2']))
+        self.k3_line.setValue(float(dist_coeff['k3']))
+        self.p1_line.setValue(float(dist_coeff['p1']))
+        self.p2_line.setValue(float(dist_coeff['p2']))
 
-        d = {
-            'k1': k1,
-            'k2': k2,
-            'k3': k3,
-            'p1': p1,
-            'p2': p2
-        }
-        return d
+        self.statusBar().showMessage(f'Loaded parameters from {self.param_file}')
+
+    def inside_circle(self, point):
+        """This is at least correct and for small list of points not too slow."""
+        index = -1
+
+        for i, p in enumerate(self.known_image_points):
+            x, y = point
+            a, b = p
+            r = 5
+
+            if (x - a)*(x - a) + (y - b)*(y - b) < r*r:
+                return i, True
+            
+        return index, False
+
 
     def in_circle(self, point):
+        """There's some kind of bud in here."""
         x, y = point
         r = 5
         index = -1
@@ -125,13 +169,13 @@ class CamTrak(QtWidgets.QMainWindow):
 
         return index, False
 
-    def add_known_image_points(self, point):
-        ind, exists = self.in_circle(point)
+    def add_known_image_points(self, point, latlonalt=None):
+        ind, exists = self.inside_circle(point)
 
         if exists:
             # Remove the point if it exists.
             del self.known_image_points[ind]
-            self.known_geo_points_tbl.removeRow(1+ind)
+            self.known_geo_points_tbl.removeRow(ind)
             print('Removed row')
         else:
             # Or add the point.
@@ -140,7 +184,18 @@ class CamTrak(QtWidgets.QMainWindow):
             # Update the table.
             row_pos = self.known_geo_points_tbl.rowCount()
             self.known_geo_points_tbl.insertRow(row_pos)
-            self.known_geo_points_tbl.setItem(row_pos, 1, QtWidgets.QTableWidgetItem(row_pos))
+            #self.known_geo_points_tbl.setItem(row_pos, 1, QtWidgets.QTableWidgetItem(row_pos))
+
+            print('row_pos: ', row_pos)
+            if latlonalt:
+                print('setting latlonalt')
+                self.known_geo_points_tbl.setItem(
+                    row_pos, 0, QtWidgets.QTableWidgetItem(str(latlonalt[0])))
+                self.known_geo_points_tbl.setItem(
+                    row_pos, 1, QtWidgets.QTableWidgetItem(str(latlonalt[1])))
+                self.known_geo_points_tbl.setItem(
+                    row_pos, 2, QtWidgets.QTableWidgetItem(str(latlonalt[2])))
+
 
         # Maybe this should be the very last thing.
         self.draw_known_points()
@@ -150,27 +205,25 @@ class CamTrak(QtWidgets.QMainWindow):
             x = int(p[0])
             y = int(p[1])
 
-            cv2.circle(self.npimage, (x, y), 5, (0, 0, 255), 2, 1)
+            cv2.circle(self.npimage, (x, y), 5, (0, 0, 255), 1, cv2.LINE_AA)
+            cv2.putText(self.npimage, f'x:{x} y:{y}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1/8, (0, 0, 255))
 
     @QtCore.pyqtSlot()
     def start_webcam(self):
-        if self.capture is None:
-            self.capture =cv2.VideoCapture(0)
-            self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-            self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.timer.start()
+        if self.camera is None:
+            self.capture = cv2.VideoCapture(0)
+            #self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+            #self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         self.timer.start()
 
     @QtCore.pyqtSlot()
     def update_frame(self):
-        ret, image = self.capture.read()
-
-        if ret:
-            self.npimage = image
-            image = cv2.flip(self.npimage, 1)
-            self.display_image(self.npimage, True)
-        else:
-            print('[ERROR] no image read from capture device')
-            return
+        ret, image = self.camera.read()
+        print(image.shape)
+        self.npimage = image
+        image = cv2.flip(self.npimage, 1)
+        self.display_image(True)
 
     @QtCore.pyqtSlot()
     def capture_image(self):
@@ -182,8 +235,9 @@ class CamTrak(QtWidgets.QMainWindow):
             cv2.imwrite(os.path.join(path, name), frame)
             self._image_counter += 1
 
-    def display_image(self, img, window=True):
+    def display_image(self, window=True):
         qformat = QtGui.QImage.Format_Indexed8
+        img = self.npimage
 
         if len(img.shape) == 3 :
             if img.shape[2] == 4:
@@ -200,15 +254,19 @@ class CamTrak(QtWidgets.QMainWindow):
         out_img = out_img.rgbSwapped()
 
         if window:
+            self.gview.setTransform(QtGui.QTransform().scale(self.zoom, self.zoom))
             self.scene.addPixmap(QtGui.QPixmap.fromImage(out_img))
-            self.gview.fitInView(QtCore.QRectF(0, 0, w, h), QtCore.Qt.KeepAspectRatio)
+            #self.gview.fitInView(QtCore.QRectF(0, 0, w, h), QtCore.Qt.KeepAspectRatio)
 
     def get_object_points(self):
         obj_points = []
 
+        print('row count: ', self.known_geo_points_tbl.rowCount())
         for i in range(self.known_geo_points_tbl.rowCount()):
+            print('on row ', i)
             lat = float(self.known_geo_points_tbl.item(i, 0).text())
             lon = float(self.known_geo_points_tbl.item(i, 1).text())
+            print('**', lat, lon)
             alt = float(self.known_geo_points_tbl.item(i, 2).text())
             x, y = self.known_image_points[i]
 
@@ -240,11 +298,28 @@ class CamTrak(QtWidgets.QMainWindow):
             'lon': lon,
             'alt': alt,
             'cx':  cx,
+            'cy': cy,
             'phi': phi,
             'theta': theta,
             'psi': psi
         }
         return cam_pos
+
+    def get_distortion_coeeficients(self):
+        k1 = float(self.k1_line.text())
+        k2 = float(self.k2_line.text())
+        k3 = float(self.k3_line.text())
+        p1 = float(self.p1_line.text())
+        p2 = float(self.p2_line.text())
+
+        d = {
+            'k1': k1,
+            'k2': k2,
+            'k3': k3,
+            'p1': p1,
+            'p2': p2
+        }
+        return d
     
     def solve_pnp(self):
         # First get the 3D model points.
@@ -252,19 +327,17 @@ class CamTrak(QtWidgets.QMainWindow):
 
         print(obj_points)
 
-
-
     def zoom_in(self):
         self.zoom *= 1.05
-        self.update_view()
+        self.display_image()
 
     def zoom_out(self):
         self.zoom /= 1.05
-        self.update_view()
+        self.display_image()
 
     def zoom_reset(self):
         self.zoom = 1
-        self.update_view()
+        self.display_image()
 
     def update_view(self):
         self.gview.setTransform(QtGui.QTransform().scale(self.zoom, self.zoom))
@@ -276,6 +349,16 @@ class CamTrak(QtWidgets.QMainWindow):
             self.zoom_in()
         elif a < 0:
             self.zoom_out()
+
+    def closeEvent(self, ev):
+        session = {}
+        if self.param_file:
+            session.update({'param file': self.param_file})
+
+        with open('previous_session.json', 'w') as f:
+            json.dump(session, f)
+
+        print('Saved session to previous_session.json')
 
 if __name__=='__main__':
     import sys
